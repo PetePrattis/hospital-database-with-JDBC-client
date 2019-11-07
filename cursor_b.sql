b67c6b4edb5abd8c53f724876aa4b01616ec554b
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-07 20:54:25 +0200

    Update cursor_b.sql

--- cursor_b.sql
+++ cursor_b.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION get_patients () --δημιουργία συνάρτησης 
-	RETURNS TABLE (--η function that returns a table με 3 στοιχεία
+CREATE OR REPLACE FUNCTION get_patients () --creation of function
+	RETURNS TABLE (--the function that returns a table with 3 elements
  		patient_name TEXT,
  		patient_surname TEXT,     
  		distinct_doctors_count INT
 	) 
 AS $$
-DECLARE --δήλωση του cursor patients ως το query από το ερώτημα d)
+DECLARE --declare the cursor patients as the query d)
 patients cursor for 
     select p.name as patient_name, p.surname as patient_surname, 
     count(distinct doctoramka) as distinct_doctors_count
@@ -13,16 +13,16 @@ patients cursor for
     where a.patientamka=p.patientamka 
     group by p.name, p.surname
     having count(*)>1;
-    i record; -- τα records του cursor
+    i record; -- the records of the cursor
 BEGIN
-   FOR i IN patients LOOP--για κάθε record στον cursor
-    	patient_name := i.patient_name ; --αποδίδω τις τιμές των records από το select του cursor στις κολώνες του function table
+   FOR i IN patients LOOP--for each record in cursor
+    	patient_name := i.patient_name ; --attribute records values from the cursor select to the function table columns
    		patient_surname := i.patient_surname;
     	distinct_doctors_count := i.distinct_doctors_count;
-              RETURN NEXT;--πηγαίνει στο επόμενο record
-   END LOOP; -- τελειώνει το loop του cursor
+              RETURN NEXT;--goes to the next record
+   END LOOP; -- end the loop of the cursor
 END; $$ 
  
 LANGUAGE 'plpgsql';
 
-select * from get_patients ()--και εμφανίζω στο τέλος αυτό το table από το function που το επιστρέφει
+select * from get_patients ()--and I print this table at the end of the function that returns it
