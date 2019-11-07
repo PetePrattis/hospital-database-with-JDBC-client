384ba7bc75f0904a67888b517ad824858a61c931
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-07 21:04:43 +0200

    Update query-2.f.sql

--- query-2.f.sql
+++ query-2.f.sql
@@ -1,7 +1,7 @@
-select avg(c.cnt) as average_diagnosed_patients from-- εύρεση του μ.ο. από
-(select dp.name as name, count(a.id) as cnt--την εύρεση  του ονόματος και του πλήθους των id 
-from appointments a, doctors d, departments dp --στους πίνακες a, d, dp
-where a.doctoramka=d.doctoramka and d.specialty=dp.id--με τα αμκα των γιατρων από τους πίνακες a, d να αντιστοιχίζονται όπως και η ειδικότητα από τους πίνακες d, dp
-and a.diagnosis is not null--και η διάγνωση να μην είναι κενή 
-group by dp.name) c--ανά τμήμα
+select avg(c.cnt) as average_diagnosed_patients from-- find the average from
+(select dp.name as name, count(a.id) as cnt--finding the name and number of ids
+from appointments a, doctors d, departments dp --in tables a, d, dp
+where a.doctoramka=d.doctoramka and d.specialty=dp.id--with the physicians' plates from tables a, d matched as the specialty from tables d, dp
+and a.diagnosis is not null--and the diagnosis is not empty 
+group by dp.name) c--per section
 
