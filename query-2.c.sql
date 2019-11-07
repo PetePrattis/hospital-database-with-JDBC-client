6e2c0495f32e8c6e0bcbec1cb107b2625c3c642f
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-07 21:01:01 +0200

    Update query-2.c.sql

--- query-2.c.sql
+++ query-2.c.sql
@@ -1,7 +1,7 @@
-select p.name as patient_name, p.surname as patient_surname, a.t as date -- βρίσκει το όνομα , επώνυμο, και ημερομηνία
-from appointments a, doctors d, patients p --από τους πίνακες a, d, p
-where a.patientamka=p.patientamka and a.doctoramka=d.doctoramka -- με τα αμκα ασθενούς και γιατρού απο τον πίνακα a να έχουν αντιστοίχιση σε αμκα απο τον πίνακα p και d
-and a.t>(select date(max(t))from appointments) - integer '30' --και η ημερομηνία στον πίνακα a για αυτά τα άμκα είναι μέχρι 30 μέρες πριν από την πιο πρόσφατη ημερομηνία
+select p.name as patient_name, p.surname as patient_surname, a.t as date -- finds name, surname, and date
+from appointments a, doctors d, patients p --from tables a, d, p
+where a.patientamka=p.patientamka and a.doctoramka=d.doctoramka -- with patient and doctor codes(amka) from table a corresponding to amka from p and d
+and a.t>(select date(max(t))from appointments) - integer '30' --and the date in table a for these abacus is up to 30 days before the most recent date
 --and a.t>current_date-30
-and d.doctoramka='3531589109644678'-- έχοντας προϋπόθεση το αμκα του γιατρου στον πίνακα d να είναι '3531589109644678'
+and d.doctoramka='3531589109644678'-- provided that the doctor's amp in table d is '3531589109644678'
 --and d.name='Kevin' and d.surname='Arnold'
