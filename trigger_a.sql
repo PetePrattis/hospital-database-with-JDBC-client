4ba883b51a2978fbc03f9b643031cf8a6d9fda14
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-07 21:06:48 +0200

    Update trigger_a.sql

--- trigger_a.sql
+++ trigger_a.sql
@@ -1,30 +1,30 @@
-CREATE FUNCTION f_appointments() RETURNS trigger AS $appointments_au$ --δημιουργία function που επιστρέφει έναν trigger
+CREATE FUNCTION f_appointments() RETURNS trigger AS $appointments_au$ --create a function that returns a trigger
     DECLARE
-        max_id          integer;--δήλωση του max id
+        max_id          integer;--max id statement
     BEGIN
     
-      select max(id)+1 into max_id -- το οποίο κάθε φορά το αρχικοποιώ με το μέγιστο id +1
-      from medicalfolders;--από τον πίνακα medicalfolders
+      select max(id)+1 into max_id -- which I initialize each time with the maximum id +1
+      from medicalfolders;--from the medicalfolders table
       
-      IF old.diagnosis IS NULL and new.diagnosis is not null THEN--αν γίνει αλλαγή της διάγνωσης και από κενή δεν είναι πλέον κενή
-    insert into medicalfolders --εισαγωγή στον πίνακα medicalfolders
+      IF old.diagnosis IS NULL and new.diagnosis is not null THEN--if the diagnosis is changed and empty it is no longer empty
+    insert into medicalfolders --introduction to the medicalfolders table
         (id,
          patientamka,
          cure,
          drug_id)
-         values --τις νέες τιμές (το νέο id που είναι τώρα το τελευταίο id, το παλιο αμκα (που ταυτίζεται με το νέο), ένα κείμενο θεραπείας, το drug id)
+         values --the new values (the new id that is now the last id, the old amka (identified with the new one), a treatment text, the drug id)
          (max_id,
           old.patientamka,
           'CURE01',
           10);
-       RETURN NEW; --επιστρέφει αυτές τις αλλαγές
-      end if; --τελειώνει το if
-    END; --και τέλος της συνάρτησης
+       RETURN NEW; --returns these changes
+      end if; --ends if
+    END; --and end of the function
 $appointments_au$ LANGUAGE plpgsql;
 
-CREATE TRIGGER appointments_au after UPDATE ON appointments --δημιουργία του trigger
+CREATE TRIGGER appointments_au after UPDATE ON appointments --create trigger
     FOR EACH ROW 
-     WHEN (OLD.DIAGNOSIS IS NULL AND NEW.DIAGNOSIS IS NOT NULL)--κάθε φορά που αλλάζει η διάγνωση από κενή σε μη κενή
-    EXECUTE PROCEDURE f_appointments();--εκτελεί την παραπάνω συνάρτηση
+     WHEN (OLD.DIAGNOSIS IS NULL AND NEW.DIAGNOSIS IS NOT NULL)--every time the diagnosis changes from blank to non-blank
+    EXECUTE PROCEDURE f_appointments();--executes the above function
 	
-	UPDATE appointments SET diagnosis = 'healthy' WHERE id = 1 and diagnosis is null;--παράδειγμα κώδικα που μπορεί να θέσει σε εκτέλεση την συνάρτηση αλλάζοντας μια κενή διάγνωση
+	UPDATE appointments SET diagnosis = 'healthy' WHERE id = 1 and diagnosis is null;--example code that can execute the function by changing a blank diagnosis
